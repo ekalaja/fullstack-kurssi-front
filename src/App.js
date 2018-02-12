316023a2a46e7582b5c2a45c14f36644843cfba2
ekalaja <eero.kalaja@helsinki.fi>
2018-02-12 17:26:33 +0100

    5.5 done

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
+import Togglable from './components/Togglable'
 
 class App extends React.Component {
   constructor(props) {
@@ -83,7 +84,7 @@ class App extends React.Component {
       console.log(blog)
       if(blog.id !== undefined) {
         this.setState({
-          announcement: 'success',
+          announcement: `a new blog ${blog.title} by ${blog.author}`,
           newTitle: '',
           newAuthor: '',
           newUrl: ''
@@ -173,7 +174,9 @@ class App extends React.Component {
 
         <p>{this.state.user.name} logged in</p>
         <button onClick={this.handleLogout}>logout</button>
-        {blogForm()}
+        <Togglable buttonLabel="Add New">
+          {blogForm()}
+        </Togglable>
         <h2>blogs</h2>
         {this.state.blogs.map(blog =>
           <Blog key={blog.id} blog={blog}/>
